a02472bb412a6401d5be01adb45bcb1421e57f38
Victor Consuegra <consuegra177@gmail.com>
2016-05-16 09:34:47 -0300

    [GROW] Funções extras
    Adicionado funções de banner,menus etc. 
    Código mais fácil de ler.

--- tabela-de-preços-wt-2016.py
+++ tabela-de-preços-wt-2016.py
@@ -1,18 +1,23 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+
 #######        Tabela de serviços WT 2016       ########
 ########      Creator - Victor Consuegra       ########
 ##########           21/02/2016               ########
-###########           Versão  1.3          #########
+###########           Versão  1.6 (C.S MODE)          #########
+
+
 
 
 #Declaração de variáveis / Operacional
 
-wolf_service = 100         # Formatação completa + Backup + Limpeza + Pasta térmica + Report completo do sistema
-clean        = 40          # Custo de uma limpeza completa
-backup       = 35          # Custo de um backup sem nenhum adicional
-wolf_search  = 75          # Custo de uma análise de sistema em busca de vírus,malwares,trojans etc.
-web_builder  = 600         # Criação de site + Domínio + Hospedagem + Manutenção mensal
-web_seo      = 200         # Manutenção de contéudo + Técnicas de SEO + Marketing cobrado por MÊS
+wolf_service = 99.99         # Formatação completa + Backup + Limpeza + Pasta térmica + Report completo do sistema
+clean        = 40            # Custo de uma limpeza completa
+backup       = 35            # Custo de um backup sem nenhum adicional
+wolf_search  = 75            # Custo de uma análise de sistema em busca de vírus,malwares,trojans etc.
+web_builder  = 600           # Criação de site + Domínio + Hospedagem + Manutenção mensal
+web_seo      = 200           # Manutenção de contéudo + Técnicas de SEO + Marketing cobrado por MÊS
 
 # Custo de necessidades durante o atendimento
 
@@ -31,116 +36,189 @@ pacote_25_pc = 2250 # 25 WORKSTATIONS R$ 250,00 em descontos
 pacote_30_pc = 2700 # 30 WORKSTATIONS R$ 300,00 em descontos
 
 # Sistemas de contratos empresariais de 3,6 e 12 meses
-# Taxa fixa de R$ 40,00 por WORKSTATION
+# Taxa fixa de R$ 69,90 por WORKSTATION
 
-deal_3_mouths  = 40 # 2 visitas por mês durante 3 meses ,  Manutenção + Diagnóstico de sistema + Limpeza
-deal_6_mouths  = 40 # 2 visitas por mês durante 6 meses ,  Manutenção + Diagnóstico de sistema + Limpeza
-deal_12_mouths = 40 # 2 visitas por mês durante 12 meses , Manutenção + Diagnóstico de sistema + Limpeza
+deal_contract  = 69.90 # 2 visitas por mês + Manutenção + Diagnóstico de sistema + Limpeza
+
+
+# Funções extras
+
+import os  # Funções do sistema que está executando o script
+import sys # Ativar a função sys.exit
+
+#Hora atual
+
+from   datetime import datetime
+now =  datetime.now()   # Principal variável para exibir a hora atual do script
+
+# Função HORA ATUAL
+
+def hora():
+    print " Brasília , %s / %s/ %i  at  %.2f  " %(now.day , now.month , now.year, now.hour)
+
+# Função WOLF BANNER
+
+def banner():
+    print  '''
+              __        _____  _     _____
+              \ \      / / _ \| |   |  ___|
+               \ /\ / / | | | | |   |_
+                \ V  V /| |_| | |___|  _|
+                 \_/\_/  \___/|_____|_|
+                           _____ _____ ____ _   _
+                          |_   _| ____/ ___| | | |
+                            | | |  _|| |   | |_| |
+                            | | | |__| |___|  _  |
+                            |_| |_____\____|_| |_|
+
+                   Contato -  wolfsecurity@protonmail.com
+                              www.wolftech.com.br
+                        Problemas com seu computador ?
+                           FALE com especialistas ! '''
+print
+print
 
 #Início da interatividade do script
 
-print '##' * 80
+def menu():
+    os.system('clear')
+
+
+banner()
+print '##' * 79
+print "     [i] Bem-vindo ao gerador de orçamentos WOLF TECHNOLOGIES                    [i] "
+print "     [i] Escolha o serviço desejado digitando números de 1 - 7                   [i] "
+print "     [i] Precisando de suporte mensal ? solicite já seu orçamento (61) 9580-7967 [i] "
+print "     [i] Digite 0 para fechar o programa                                         [i] "
+print '--' * 81
 print
-print "     [1] Limpeza + Formatação + Backup + Report completo do sistema "
-print "     [2] Limpeza completa "
-print "     [3] Backup de dados"
-print "     [4] Análise de sistema + remoção de vírus,trojan, malware etc. "
-print "     [5] Criação de sites pessoais ou profissionais "
-print "     [6] Impulso de web SEO + Marketing digital "
+print "     [1] -Limpeza , Formatação , Backup , Report completo do sistema -  [+] "
+print "     [2] -Limpeza completa - Desktop & Notebook                      -  [+] "
+print "     [3] -Backup de dados                                            -  [+] "
+print "     [4] -Análise completa do sistema                                -  [+] "
+print "     [5] -Criação e hospedagem de sites profissionais                -  [+] "
+print "     [6] -Técnicas de SEO, criação de contéudo                       -  [+] "
+print "     [7] -Contratos corporativos 3,6 e 12 meses                      -  [+] "
+print "     [0] -Sair do programa                                           -  [-] "
 print
-print '##' * 80
-service = input (" Escolha o plano desejado ")
+
+print " Start script at "
+
+hora()                     # Hora,dia e ano atual
+
 print
-print '--' * 80
 
-# Início dos cálculos baseados na escolha de serviço
+# Função menu , principal função desta atualização
 
-# Opção [1] Limpeza + Formatação + Backup + Report de sistema
-# Informativo sobre os descontos promocionais de acordo com o número de computadores
+def menu_opcao():
 
-if service == 1 :
-    pc = input (" Quantos computadores serão atendidos ?  ")
-    print
-    total_pc = wolf_service * pc
-    print       " O Custo total do serviço é de: R$ %.2f  " % total_pc
-    if pc == 3 :
-        print   " O Valor do pacote de 3 WORKSTATIONS é de : R$ %.2f    "  % pacote_3_pc
-        print '--' *  80
+    opcao = raw_input ("[START] - Selecione uma opção [0-7] [!]    ")
+    if opcao ==   '1':
         print
-    if pc == 5 :
-        print   " O Valor do pacote de 5 WORKSTATIONS é de : R$ %.2f    "  % pacote_5_pc
-        print '--'  * 80
+        print                  " [!] O valor para cada computador é de R$ 100,00               [!] "
+        print                  " [!] Consultar tabela de preços para atendimentos corporativos [!] "
+        print                  " [!]                    www.wolftech.com.br                    [!] "
         print
-    if pc == 10 :
-        print   " O valor do pacote de 10 WORKSTATIONS é de : R$ %.2f   "  % pacote_10_pc
-        print '--' *  80
+        work_pc       =  input ( " Há quantos computadores no local  ?  " )
+        total_pc      = work_pc * wolf_service
+        print                  ( " O VALOR DO SERVIÇO FICOU EM -  [  R$ %.2f   ]" ) % total_pc
+        print                  ( " Para mais informações sobre parcelamentos e pagamentos ")
+        print '--' * 81
+        print                  ( " Consulte nossos planos de suporte corporativo - www.wolftech.com.br ")
+        print                  ( " Preços mais leves de acordo com o número de WORKSTATIONS ")
         print
-    if pc == 15 :
-        print   " O valor do pacote de 15 WORKSTATIONS é de : R$ %.2f   "  % pacote_15_pc
-        print '--' *  80
+
+
+
+
+    elif opcao == '2':
         print
-    if pc == 20 :
-        print   " O valor do pacote de 20 WORKSTATIONS é  de : R$ %.2f    " % pacote_20_pc
-        print '--' *  80
+        print                   " [!] O valor para cada computador é de R$ 40,00                [!] "
+        print                   " [!] Consultar tabela de preços para atendimentos corporativos [!] "
         print
-    if pc == 25 :
-        print   " O valor do pacote de 25 WORKSTATIONS é de  : R$ %.2f    " % pacote_25_pc
-        print '--' *  80
+        work_pc2      = input  ( " Há quantos computadores no local  ?   ")
+        total_pc2     = work_pc2 * clean
+        print                  (" O VALOR DO SERVIÇO FICOU EM -   R$ %.2f    ") % total_pc2
         print
-    if pc == 30 :
-        print   " O valor do pacote de 30 WORKSTATIONS é de  : R$ %.2f    " % pacote_30_pc
-print '==' * 80
 
 
-#Opção [2] Limpeza completa
-
-if service == 2 :
-    pc_2      = input (" Quantos computadores serão atendidos ?  ")
-    total_pc2 = clean * pc_2
-    print              "O Custo total do serviço é de: R$ %.2f  " % total_pc2
-    print '==' * 80
-
-
-
-#Opção [3] Backup de dados
+    elif opcao == '3':
+        print
+        print                   " [!] O valor para cada computador é de R$ 35,00                [!] "
+        print                   " [!] Consultar tabela de preços para atendimentos corporativos [!] "
+        print
+        work_pc3      = input  ( " Há quantos computadores no local  ?   ")
+        total_pc3     = work_pc3 * backup
+        print                  (" O VALOR DO SERVIÇO FICOU EM -  R$ %.2f    ") % total_pc3
+        print
 
 
-if service == 3 :
-    pc_3      = input (" Quantos computadores serão atendidos ?  ")
-    total_pc3 = backup * pc_3
-    print              " O custo total do serviço é de : R$ %.2f  " % total_pc3
-    print '==' * 80
+    elif opcao == '4':
+        print
+        print                   " [!] O valor para cada computador é de R$ 75,00                [!] "
+        print                   " [!] Consultar tabela de preços para atendimentos corporativos [!] "
+        print
+        work_pc4      = input  ( " Há quantos computadores no local  ?   ")
+        total_pc4     = work_pc4 * wolf_search
+        print                  (" O VALOR DO SERVIÇO FICOU EM -   R$ %.2f    ") % total_pc4
+        print
 
 
-
-#Opção [4] Análise e diagnóstico de sistema para possivéis ameaças
-
-
-if service == 4 :
-    pc_4      = input  (" Quantos computadores serão atendidos ?  ")
-    total_pc4 = wolf_search * pc_4
-    print               " O custo total do serviço é de : R$ %.2f  " % total_pc4
-    print '==' * 80
-
-
-
-#Opção [5] Criação de web sites pessoais e profissionais
+    elif opcao == '5':
+        print
+        print                   " [!] O valor para desenvolvimento e hospedagem é de R$ 600,00     [!] "
+        print                   " [!] Site seguros,responsivos,rapidos e bonitos !                 [!] "
+        print                   " [!] Sites feitos sob medida, e atendendo os critérios do cliente [!] "
+        print
+        work_pc5      = input  ( " Quantos websites serão criados e hospedados ?   ")
+        total_pc5     = work_pc5 * web_builder
+        print                  (" O VALOR DO SERVIÇO FICOU EM -  R$ %.2f   ") % total_pc5
+        print
 
 
-if service == 5 :
-    web       = input     (" Quantos websites serão criados  ? ")
-    total_web = web_builder * web
-    print                  " O custo total do web site é de : R$ %.2f   " % total_web
-    print '==' * 80
+    elif opcao == '6':
+        print
+        print                   " [!] O valor mensal a ser pago é de R$ 200,00                     [!] "
+        print                   " [!] Consultar tabela de preços para atendimentos corporativos    [!] "
+        print                   " [!] O valor só será pago se as metas de tráfegos forem atingidas [!] "
+        print
+        work_pc6      = input  ( " Necessita de suporte SEO por quantos meses  ?   ")
+        total_pc6     = work_pc6 * web_seo
+        print                  (" O VALOR DO SERVIÇO FICOU EM -   R$ %.2f   ") % total_pc6
+        print
 
 
-#Opção [6] Impulso de WEB SEO + Marketing digital
+    elif opcao == '7':
+        print '--' * 81
+        print                   "  [!] Os serviços prestados via contrato funcionarão da seguinte forma [!]  "
+        print                   "  [!] 2 visitas por mês + diagnóstico de sistema + limpeza completa    [!]  "
+        print
+        print                   "  [!] O valor para cada  WORKSTATION é de R$ 69,90                     [!]  "
+        print
+        work_pc7      = input  ( " Há quantos computadores na empresa  ?       ")
+        total_pc7     = (work_pc7 * deal_contract)
+        print
+        contract      = input  ( " Digite o tempo de suporte [3] , [6] , [12]  ")
+        print
+
+        if   contract  == 3:
+            print " O VALOR TOTAL DO CONTRATO MENSAL É DE -  R$ %.2f    " % total_pc7
+            print
+        elif contract  == 6:
+            print " O valor total do contrato mensal é de R$ %.2f   " % total_pc7
+            print
+        elif contract  == 12:
+            print " O valo total do contrato  mensal é de R$ %.2f    " % total_pc7
+            print
+
+    elif opcao == '0':
+        sys.exit()
 
 
-if service  == 6 :
-    seo        = input     (" Por quantos meses irá precisar de suporte SEO ?   ")
-    total_web2 = web_seo * seo
-    print                 " O custo total do impulso de SEO será de : R$ %.2f   " %total_web2
-    print '==' * 80
+    else:
+        menu()
+
+menu_opcao()  # Inicia o script
+
 
 
